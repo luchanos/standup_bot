3cfa1bcd0991ae5d004dbf3e64b045d29b01b6a0
luchanos <lukash_91@mail.ru>
2022-02-02 01:46:15 +0300

    бот

--- cron_reminder.py
+++ cron_reminder.py
@@ -1,5 +1,14 @@
 import requests
+import json
+from datetime import date
+from envparse import env
 
-TOKEN = "5200212331:AAHYPoA56yoKQ9Gf0lQuaxUoo3FtXdIY7qg"
-ADMIN_CHAT_ID = 362857450
-requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={ADMIN_CHAT_ID}&text=завожу бота")
+TOKEN = env.str("TOKEN", default="")
+ADMIN_CHAT_ID = 0
+
+with open("/Users/nnsviridov/PycharmProjects/AcademicProjects/standup_bot/standup_log.json", "r") as f_o:
+    data_from_json = json.load(f_o)
+    for user_id in data_from_json:
+        if data_from_json[user_id]["last_updated_dt"] != str(date.today()):
+            chat_id = data_from_json[user_id]["chat_id"]
+            requests.get(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text=Привет! Как насчёт того, чтобы постендапиться?)")
