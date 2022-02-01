fee39c053b89c00ec682f7147a746f191e8014c3
luchanos <lukash_91@mail.ru>
2022-02-01 23:46:06 +0300

    бот

--- standup_bot.py
+++ standup_bot.py
@@ -17,7 +17,7 @@ def standup_speech(message):
     data_from_json[str(date.today())] = message.text
 
     with open("standup_log.json", "w") as f_o:
-        json.dump(data_from_json, f_o)
+        json.dump(data_from_json, f_o, indent=4, ensure_ascii=False)
 
     bot.reply_to(message, text="Отлично! Хорошего дня!")
 
@@ -26,6 +26,24 @@ def standup_speech(message):
 def standuper(message):
     bot.reply_to(message, text="Чем занимался вчера? Чем будешь заниматься сегодня? Есть ли какие-нибудь трудности?")
     bot.register_next_step_handler(message, standup_speech)
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    chat_id = message.chat.id
+    with open("standup_log.json", "r") as f_o:
+        data_from_json = json.load(f_o)
+    if str(user_id) not in data_from_json:
+        data_from_json[user_id] = {
+            "last_updated_dt": None,
+            "chat_id": chat_id
+        }
+        with open("standup_log.json", "w") as f_o:
+            json.dump(data_from_json, f_o, indent=4, ensure_ascii=False)
+        bot.reply_to(message, text="Добро пожаловать!")
+    else:
+        bot.reply_to(message, text="И снова здравствуйте!")
 
 
 while True:
